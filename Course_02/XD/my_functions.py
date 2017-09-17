cba0a9719d9ab43e751b137368123538ccf018ca
thhuang <thhuang@caece.net>
2017-09-17 14:40:06 +0800

    Edited notes. (Don't know how to get finishing time.)

--- Course_02/XD/my_functions.py
+++ Course_02/XD/my_functions.py
@@ -1,3 +1,7 @@
+from tqdm import tqdm
+from time import sleep
+
+
 class Stack:
     def __init__(self):
         self._storage = []
@@ -8,15 +12,95 @@ class Stack:
     def pop(self):
         return self._storage.pop()
 
+    def peek(self):
+        return self._storage[-1]
+
     def is_empty(self):
         return len(self._storage) == 0
 
-class Vertex:
-    def __init__(self):
-        self._label = False
 
-    def label(self):
-        self._label = True
+class Graph:
+    def __init__(self, filename):
+        self._vertices = dict()
+        self._vertices_rev = dict()  # reversal
+        self._territory = dict()
+        self._edges = list()
+        self._leader = dict()
+        self._finish = dict()
+        self._t = 0  # finishing time
+        self._s = None  # leader
 
-    def is_labeled(self):
-        return self._label
+        for line in tqdm(open(filename)):
+            v, w = line.split()
+            v = int(v)
+            w = int(w)
+
+            # add adjacent list
+            if not self._vertices.get(v):
+                self._territory[v] = False
+                self._vertices[v] = [w]
+            else:
+                self._vertices[v].append(w)
+            if not self._vertices_rev.get(w):
+                self._vertices_rev[w] = [v]
+            else:
+                self._vertices_rev[w].append(v)
+
+            # include sink nodes
+            if not self._vertices.get(w):
+                self._territory[w] = False
+                self._vertices[w] = []
+            if not self._vertices_rev.get(v):
+                self._vertices_rev[v] = []
+
+
+
+            self._edges.append((v, w))
+
+        sleep(0.1)
+        print('n = ' + str(len(self._vertices)) + ', m = ' + str(len(self._edges)))
+        print('{:-^50}'.format(''))
+
+    @property
+    def vertices(self):
+        return self._vertices
+
+    @property
+    def edges(self):
+        return self._edges
+
+    def is_explored(self, v):
+        return self._territory[v]
+
+    def explore(self, v):
+        self._territory[v] = True
+
+    def reset_territory(self):
+        for v in self._territory:
+            self._territory[v] = False
+
+    def dfs(self, s, reverse=False):
+        self._leader[s] = []
+        stk = Stack()
+        stk.push(s)
+
+        while not stk.is_empty():
+            v = stk.pop()
+            print(v)
+            if not self.is_explored(v):
+                self.explore(v)
+                self._leader[s].append(v)
+                if reverse:
+                    for w in self._vertices_rev[v]:
+                        stk.push(w)
+                else:
+                    for w in self._vertices[v]:
+                        stk.push(w)
+
+    def dfs_loop(self, reverse=False):
+        for i in reversed(list(self._vertices.keys())):
+            if not self.is_explored(i):
+                self._s = i
+                self.dfs(self._s, reverse)
+                #print(str(self._s) + ' leads ' + str(len(self._leader[self._s])))
+
--- Course_02/assignment01.py
+++ Course_02/assignment01.py
@@ -1,2 +1,12 @@
-import XD.my_functions as XD
+import XD.my_functions as my
+import random
+
+G = my.Graph('large_files/SCC_test01.txt')
+#G = my.Graph('large_files/SCC.txt')
+
+#s = random.choice(list(G.vertices.keys()))
+#print('s = ' + str(s) + ', ' + str(G.vertices[s]))
+
+G.dfs_loop()
+
 
