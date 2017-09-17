e9186ebd4751236c070e42c6912afb6e525c9460
thhuang <thhuang@caece.net>
2017-09-17 09:35:27 +0800

    Edited my functions

--- Course_02/XD/my_functions.py
+++ Course_02/XD/my_functions.py
@@ -10,3 +10,13 @@ class Stack:
 
     def is_empty(self):
         return len(self._storage) == 0
+
+class Vertex:
+    def __init__(self):
+        self._label = False
+
+    def label(self):
+        self._label = True
+
+    def is_labeled(self):
+        return self._label
