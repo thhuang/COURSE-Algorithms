240ca65a4279f2c098250fde21a68e750968dbb5
thhuang <thhuang@caece.net>
2017-09-29 22:46:36 +0800

    Finished assignment 3

--- Course_03/assignment03-03.py
+++ Course_03/assignment03-03.py
@@ -12,8 +12,30 @@ except Exception as e:
     sys.exit()
 
 # initialization
-maximum_weight = list()
-index = 0
-for weight in file:
-    index += 1
+w = int(file.readline())
+weight = [0, w]
+maximum_weight = [0, w]
 
+# main loop:
+# maximum_weight[i] = max{ maximum_weight[i-1], maximum_weight[i-2] + wi }
+for w in file:
+    maximum_weight.append(max(maximum_weight[-1], maximum_weight[-2] + int(w)))
+    weight.append(int(w))
+# compute max-weight independent set
+mwis = set()
+i = 1000
+while i > 0:
+    if maximum_weight[i - 2] + weight[i] > maximum_weight[i - 1]:
+        mwis.add(i)
+        i -= 2
+    else:
+        i -= 1
+
+# compute the answer
+ans = ''
+for i in [1, 2, 3, 4, 17, 117, 517, 997]:
+    if i in mwis:
+        ans += '1'
+    else:
+        ans += '0'
+print('Answer:', ans)
