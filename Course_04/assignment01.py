6ee5bfa0d115b34c4b85b2a0d749182090182c55
thhuang <thhuang@caece.net>
2017-10-03 16:56:20 +0800

    Finished assignment 1

--- Course_04/assignment01.py
+++ Course_04/assignment01.py
@@ -2,7 +2,6 @@ import XD.my_functions as my
 
 G = my.Graph('large_files/g3.txt', different_length_direct=True)
 #G = my.Graph('large_files/g_test01.txt', different_length_direct=True)
-#G = my.Graph('large_files/dijkstraData.txt', different_length=True)
 
 G.johnson_all_pairs_shortest_path()
 
