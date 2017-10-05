017917b0c52b787e7cea6ff0a60037606d3f02b4
thhuang <thhuang@caece.net>
2017-10-05 23:11:22 +0800

    Working on assignment 2

--- Course_04/assignment02.py
+++ Course_04/assignment02.py
@@ -31,9 +31,9 @@ for v in V:
     X.append(v.x)
     Y.append(v.y)
 d_max = ((max(X) - min(X))**2 + (max(Y) - min(Y))**2) ** 0.5
-plt.scatter(X, Y)
-plt.show()
-cutoff = float(input('Enter the cutoff: '))
+#plt.scatter(X, Y)
+#plt.show()
+cutoff = 0.3 #float(input('Enter the cutoff: '))
 cutoff *= d_max
 
 # initialize
@@ -47,33 +47,41 @@ for S in combinations(range(n), 1):
         A[(S, 0)] = 0
 all_sets.append(A)
 
+# distance matrix
+D = list()
+for i in range(n):
+    D_row = list()
+    for j in range(n):
+        D_row.append(V[i].distance(V[j]))
+    D.append(D_row)
+
 # main loop
 time.sleep(0.1)
+t_main = time.time()
 for m in range(2, n+1):  # m = sub-problem size
     t0 = time.time()
-    A = dict()
+    A_new = dict()
     for S in combinations(range(n), m):
         if S[0] == 0:
-            for j in S[1:]:
-                sub_prob = list()
-                for k in S:
-                    if k != j and V[k].distance(V[j]) < cutoff:
-                        S_sub = tuple(v for v in S if v != j)
-                        if len(S_sub) == 1 or k != 0:
-                            sub_prob.append(all_sets[m-1][(S_sub, k)] + V[k].distance(V[j]))
-                A[(S, j)] = min(sub_prob) if len(sub_prob) else inf
+            for i in range(1, m):
+                j = S[i]
+                S_sub = S[:i] + S[i + 1:]
+                sub_prob = inf
+                for k in S_sub:
+                    if D[k][j] < cutoff and (S_sub, k) in A and sub_prob > A[(S_sub, k)] + D[k][j]:
+                        sub_prob = A[(S_sub, k)] + D[k][j]
+                A_new[(S, j)] = sub_prob
     print(m, round(min(A.values()), 2), round(time.time() - t0, 2))
-    if min(A.values()) == inf:
+    if min(A_new.values()) == inf:
         print('Try a larger cutoff!')
         exit()
-    all_sets.append(A)
-    all_sets[m - 1] = []
+    A = A_new
 
 time.sleep(0.1)
 S = tuple(range(n))
 tours = list()
 for j in S[1:]:
-    tours.append(all_sets[n][(S, j)] + V[j].distance(V[0]))
+    tours.append(A[(S, j)] + V[j].distance(V[0]))
 
-print('The minimum cost of a traveling salesman tour is', min(floor(tours)))
-
+print(round(time.time() - t_main, 2))
+print('The minimum cost of a traveling salesman tour is', floor(min(tours)))
